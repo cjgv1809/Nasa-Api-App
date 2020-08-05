358b615bf5b74c9763a9fc3533c11808c4ff87a5
Carlos <carlos_gomes1809@hotmail.com>
2020-08-05 16:47:02 -0300

    Updates

---
+++ precache-manifest.06d0e2e4d5ad665e6b2b3a81815c8318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7defbc9ea47b44b08b401cf63f674ae",
+    "url": "/nasa-api-app/index.html"
+  },
+  {
+    "revision": "0fa06f20c77766658523",
+    "url": "/nasa-api-app/static/css/main.b72e5bf1.chunk.css"
+  },
+  {
+    "revision": "ad9237ddb38533b14a1b",
+    "url": "/nasa-api-app/static/js/2.9b4bae84.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/nasa-api-app/static/js/2.9b4bae84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa06f20c77766658523",
+    "url": "/nasa-api-app/static/js/main.5cdccf32.chunk.js"
+  },
+  {
+    "revision": "3c8884748d4e52eb7173",
+    "url": "/nasa-api-app/static/js/runtime-main.af2ba722.js"
+  },
+  {
+    "revision": "c714a5d1b2deccbd25a329e3d7a5e3bc",
+    "url": "/nasa-api-app/static/media/NasaPic.c714a5d1.jpg"
+  }
+]);
